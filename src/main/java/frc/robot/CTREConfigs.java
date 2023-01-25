774679b20e61ad6087b30f4aee20e4e084fd5434
Y TEAM <yer.3211@gmail.com>
2023-01-26 00:01:44 +0200

    changes for CANCoder

--- src/main/java/frc/robot/CTREConfigs.java
+++ src/main/java/frc/robot/CTREConfigs.java
@@ -2,21 +2,21 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
-import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
-import com.ctre.phoenix.sensors.AbsoluteSensorRange;
+// import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration; |
+// import com.ctre.phoenix.sensors.AbsoluteSensorRange;            | if you use talon SRX mag Encoder
+// import com.ctre.phoenix.sensors.SensorInitializationStrategy;   |
+// import com.ctre.phoenix.sensors.SensorTimeBase;                 |
 import com.ctre.phoenix.sensors.CANCoderConfiguration;
-import com.ctre.phoenix.sensors.SensorInitializationStrategy;
-import com.ctre.phoenix.sensors.SensorTimeBase;
 
 public final class CTREConfigs {
     public TalonFXConfiguration swerveAngleFXConfig;
     public TalonFXConfiguration swerveDriveFXConfig;
-    public TalonSRXConfiguration swerveCanCoderConfig;
+    public CANCoderConfiguration swerveCanCoderConfig;
 
     public CTREConfigs(){
         swerveAngleFXConfig = new TalonFXConfiguration();
         swerveDriveFXConfig = new TalonFXConfiguration();
-        swerveCanCoderConfig = new TalonSRXConfiguration();
+        swerveCanCoderConfig = new CANCoderConfiguration();
 
         /* Swerve Angle Motor Configurations */
         SupplyCurrentLimitConfiguration angleSupplyLimit = new SupplyCurrentLimitConfiguration(
@@ -46,10 +46,11 @@ public final class CTREConfigs {
         swerveDriveFXConfig.openloopRamp = Constants.Swerve.openLoopRamp;
         swerveDriveFXConfig.closedloopRamp = Constants.Swerve.closedLoopRamp;
         
-        /* Swerve CANCoder Configuration */
-        //swerveCanCoderConfig.absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
-        //swerveCanCoderConfig.sensorDirection = Constants.Swerve.canCoderInvert;
-        //swerveCanCoderConfig.initializationStrategy = SensorInitializationStrategy.BootToAbsolutePosition;
-        //swerveCanCoderConfig.sensorTimeBase = SensorTimeBase.PerSecond;
+    //     /* Swerve CANCoder Configuration */
+    //     //swerveCanCoderConfig.absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
+    //     //swerveCanCoderConfig.sensorDirection = Constants.Swerve.canCoderInvert;
+    //     //swerveCanCoderConfig.initializationStrategy = SensorInitializationStrategy.BootToAbsolutePosition;
+    //     //swerveCanCoderConfig.sensorTimeBase = SensorTimeBase.PerSecond;
+    // }
     }
 }
