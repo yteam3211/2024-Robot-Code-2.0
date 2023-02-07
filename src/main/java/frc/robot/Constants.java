19adb375388af450c9f9f67cfcc96cfa99bd3097
noya <yer.3211@gmail.com>
2023-02-07 22:46:10 +0200

    collect

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,8 +16,8 @@ public final class Constants {
         public static final int SHOOTING_MOTOR = 15;
         public static final int DOWN_MICROSWITCH = 0; 
         public static final int UP_MICROSWITCH = 0;
-        public static final int LEADER_COLLECT_MOTOR = 0;
-        public static final int COLLECT_MOTOR = 0;
+        public static final int RIGHT_LEADER_COLLECT_MOTOR = 17;
+        public static final int LEFT_COLLECT_MOTOR = 16;
         public static final int CLOSE_MICROSWITCH = 0;
 
     public static final double stickDeadband = 0.01;
