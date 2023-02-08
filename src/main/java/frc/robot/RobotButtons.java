badf913369da368be778376c5d1fffb06e86d9be
Y TEAM <yer.3211@gmail.com>
2023-02-08 18:28:43 +0200

    change joystick axis

--- src/main/java/frc/robot/RobotButtons.java
+++ src/main/java/frc/robot/RobotButtons.java
@@ -21,8 +21,10 @@ public class RobotButtons {
         swerve.setDefaultCommand(
             new TeleopSwerve(
                 swerve, 
-                () -> driver.getRawAxis(XboxController.Axis.kLeftY.value), // up & down
-                () -> driver.getRawAxis(XboxController.Axis.kLeftX.value),  // left & right 
+                // () -> driver.getRawAxis(XboxController.Axis.kLeftY.value), // up & down
+                () -> driver.getRawAxis(0), // up & down
+                // () -> driver.getRawAxis(XboxController.Axis.kLeftX.value),  // left & right 
+                () -> driver.getRawAxis(1),  // left & right 
                 () -> driver.getRawAxis(XboxController.Axis.kRightX.value), // rotation
                 () -> robotCentric.getAsBoolean()
             )
