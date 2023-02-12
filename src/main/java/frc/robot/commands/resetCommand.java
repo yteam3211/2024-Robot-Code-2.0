a2fdf9d1b144188d6f9313f87493e5ea0794a5bc
Y TEAM <yer.3211@gmail.com>
2023-02-12 23:33:53 +0200

    comment reset command

--- src/main/java/frc/robot/commands/resetCommand.java
+++ src/main/java/frc/robot/commands/resetCommand.java
@@ -1,54 +1,54 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+// package frc.robot.commands;
 
-import frc.robot.subsystems.CollectSubsyste;
-import frc.robot.subsystems.ShootingSubsystem;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+// import frc.robot.subsystems.CollectSubsyste;
+// import frc.robot.subsystems.ShootingSubsystem;
+// import edu.wpi.first.wpilibj2.command.CommandBase;
 
-/** An example command that uses an example subsystem. */
-public class reSetCommand extends CommandBase {
-  private final ShootingSubsystem shootingSubsystem;
-  private final CollectSubsyste collectSubsyste;
+// /** An example command that uses an example subsystem. */
+// public class resetCommand extends CommandBase {
+//   private final ShootingSubsystem shootingSubsystem;
+//   private final CollectSubsyste collectSubsyste;
 
   
   
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public reSetCommand(ShootingSubsystem shootingSubsystem, CollectSubsyste collectSubsyste) {
-    this.shootingSubsystem = shootingSubsystem;
-    this.collectSubsyste = collectSubsyste;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(shootingSubsystem,collectSubsyste);
-  }
+//   /**
+//    * Creates a new ExampleCommand.
+//    *
+//    * @param subsystem The subsystem used by this command.
+//    */
+//   public reSetCommand(ShootingSubsystem shootingSubsystem, CollectSubsyste collectSubsyste) {
+//     this.shootingSubsystem = shootingSubsystem;
+//     this.collectSubsyste = collectSubsyste;
+//     // Use addRequirements() here to declare subsystem dependencies.
+//     addRequirements(shootingSubsystem,collectSubsyste);
+//   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+//   // Called when the command is initially scheduled.
+//   @Override
+//   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    shootingSubsystem.resetEncoder();
-    collectSubsyste.reSetEncoder();
-  }
+//   // Called every time the scheduler runs while the command is scheduled.
+//   @Override
+//   public void execute() {
+//     shootingSubsystem.resetEncoder();
+//     collectSubsyste.reSetEncoder();
+//   }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+//   // Called once the command ends or is interrupted.
+//   @Override
+//   public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
+//   // Returns true when the command should end.
+//   @Override
+//   public boolean isFinished() {
 
-  return false;
+//   return false;
 
  
-}
-}
+// }
+// }
