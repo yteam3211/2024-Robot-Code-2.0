43b9d52481c090eefee02c4b5c3d5133253383e5
Y TEAM <yer.3211@gmail.com>
2023-02-21 00:24:06 +0200

    change reset command to an instant command

--- src/main/java/frc/robot/commands/resetCommand.java
+++ src/main/java/frc/robot/commands/resetCommand.java
@@ -4,55 +4,32 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.CollectSubsyste;
 import frc.robot.subsystems.ShootingSubsystem;
 import frc.robot.subsystems.armSubsystem;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 
-/** An example command that uses an example subsystem. */
-public class resetCommand extends CommandBase {
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class ResetCommand extends InstantCommand {
   private final ShootingSubsystem shootingSubsystem;
   private final CollectSubsyste collectSubsyste;
   private final armSubsystem armSubsystem;
 
-  
-  
-
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public resetCommand(ShootingSubsystem shootingSubsystem, CollectSubsyste collectSubsyste,  armSubsystem armSubsystem) {
+  public ResetCommand(ShootingSubsystem shootingSubsystem, CollectSubsyste collectSubsyste,  armSubsystem armSubsystem) {
     this.shootingSubsystem = shootingSubsystem;
     this.collectSubsyste = collectSubsyste;
     this.armSubsystem = armSubsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(shootingSubsystem,collectSubsyste);
+
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
+  public void initialize() {
     shootingSubsystem.resetEncoder();
     collectSubsyste.reSetEncoder();
     armSubsystem.resetEncoder();
   }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-
-  return false;
-
- 
-}
 }
