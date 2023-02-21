0ca2721463952346054debb535dce409a1e8451f
Y TEAM <yer.3211@gmail.com>
2023-02-21 06:36:21 +0200

    add initShooting

--- src/main/java/frc/robot/commands/initShooting.java
+++ src/main/java/frc/robot/commands/initShooting.java
@@ -4,29 +4,50 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ShootingSubsystem;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class initShooting extends InstantCommand {
+public class InitShooting extends CommandBase {
   private final ShootingSubsystem shootingSubsystem;
 
-  public initShooting(ShootingSubsystem shootingSubsystem) {
-    this.shootingSubsystem = shootingSubsystem;
+
+
+  /** Creates a new InitShooting. */
+  public InitShooting(ShootingSubsystem shootingSubsystem) {
+    this.shootingSubsystem = shootingSubsystem ;
+
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(shootingSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    if(shootingSubsystem.GetPosition() <= shootingSubsystem.Min){
-  shootingSubsystem.setOutput(0);
-  shootingSubsystem.resetEncoder();
-  }else{
-    shootingSubsystem.setOutput(-0.1);
- 
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    System.out.println("rrrrr");
+    shootingSubsystem.setOutput(-0.15);
+     
+      
+ }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    shootingSubsystem.setOutput(0);
+      shootingSubsystem.resetEncoder();
+    shootingSubsystem.changeDefault();
   }
-}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if(shootingSubsystem.GetPosition() <= shootingSubsystem.Min){
+      return true;
+    }
+    return false;
+    
+  }
 }
