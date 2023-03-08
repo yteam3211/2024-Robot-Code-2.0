5a0a1c01f1bfd364bd309c67fe63841cb673d6f8
Y TEAM <yer.3211@gmail.com>
2023-03-09 00:28:30 +0200

    update auto

--- src/main/java/frc/robot/commands/autoCommands/FarFromHumanRampa.java
+++ src/main/java/frc/robot/commands/autoCommands/FarFromHumanRampa.java
@@ -4,62 +4,58 @@
 
 package frc.robot.commands.autoCommands;
 
+import frc.robot.autos.FarFromHumanAndRamp;
+import frc.robot.autos.next2human;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.RobotButtons;
-import frc.robot.autos.centerToRampa;
 import frc.robot.subsystems.Swerve;
+import frc.robot.subsystems.armCollectSubsystem;
 import frc.robot.commands.timercommand.timeSetPointCollectCommand;
 import frc.robot.subsystems.CollectSubsystem;
 import frc.robot.subsystems.CartridgeSubsystem;
 import frc.robot.subsystems.armSubsystem;
 import frc.robot.subsystems.collectWheels;
 import frc.robot.subsystems.shootingSubsystem;
+import frc.util.vision.Limelight;
+import frc.robot.commands.ArmCollectCommand;
 import frc.robot.commands.BalanceCommand;
+import frc.robot.commands.LimelightCommand;
 import frc.robot.commands.ShootingCommand;
 // import frc.robot.commands.ClosingCollectGroupCommand;
+import frc.robot.commands.resetCommand;
 import frc.robot.commands.shootingOutputCommand;
 import frc.robot.commands.timercommand.TimerArmPosition;
 import frc.robot.commands.timercommand.moveInParallel;
 
-
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class center extends SequentialCommandGroup {
-  /** Creates a new atuo1. */
+    // addCommands(new FooCommand(), new BarCommand());
+public class FarFromHumanRampa extends SequentialCommandGroup {
   private Swerve s_Swerve;
-  private shootingSubsystem ShootingSubsystem;
   private CartridgeSubsystem cartridgeSubsystem;
   private CollectSubsystem collectSubsystem;
   // private ClosingCollectGroupCommand collectGroupCommand;
   private timeSetPointCollectCommand timeSetPointCollectCommand;
   private TimerArmPosition TimerArmPosition;
   private collectWheels collectWheels;
-  
-
-  
-  public center(Swerve swerve,
-  armSubsystem armSubsystem,
+  private armCollectSubsystem armCollectSubsystem;
+  private shootingSubsystem shootingSubsystem;
+  private Limelight limelight;
+  /** Creates a new FarFromHuman. */
+  public FarFromHumanRampa(Swerve swerve,
   CollectSubsystem collectSubsystem,
-  CartridgeSubsystem cartridgeSubsystem,
-  collectWheels collectWheels, shootingSubsystem shootingSubsystem
-) {
+  CartridgeSubsystem ShootingSubsystem,
+  collectWheels collectWheels, armCollectSubsystem armCollectSubsystem, Limelight limelight, shootingSubsystem shootingSubsystem) {
     this.s_Swerve = swerve;
     this.collectSubsystem = collectSubsystem;
     this.cartridgeSubsystem = cartridgeSubsystem;
     this.collectWheels = collectWheels;
-    this.ShootingSubsystem = shootingSubsystem;
-    
-  
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    
-  
-    addCommands(new InstantCommand(() -> swerve.zeroGyro()), new ShootingCommand(shootingSubsystem, cartridgeSubsystem, 6000, 0.4),
-    new moveInParallel(swerve, collectSubsystem, collectWheels, centerToRampa.getAutoCommand(s_Swerve), 290, 7, 2),
-    // centerToRampa.getAutoCommand(s_Swerve),
+    this.limelight = limelight;
+    this.shootingSubsystem = shootingSubsystem;
+    this.armCollectSubsystem = armCollectSubsystem;
+    addCommands(new InstantCommand(() -> swerve.zeroGyro()), new resetCommand(shootingSubsystem, collectSubsystem, armCollectSubsystem, cartridgeSubsystem),
+    new ShootingCommand(shootingSubsystem, cartridgeSubsystem, 6000, 0.4),
+    new moveInParallel(swerve, collectSubsystem, collectWheels, FarFromHumanAndRamp.getAutoCommand(swerve), 290, 5, 1),
     new BalanceCommand(swerve)
-    );  
+    );
   }
 }
--- src/main/java/frc/robot/commands/autoCommands/Next2HumanCommand.java
+++ src/main/java/frc/robot/commands/autoCommands/Next2HumanCommand.java
@@ -12,21 +12,20 @@ import frc.robot.subsystems.Swerve;
 import frc.robot.subsystems.armCollectSubsystem;
 import frc.robot.commands.timercommand.timeSetPointCollectCommand;
 import frc.robot.subsystems.CollectSubsystem;
-import frc.robot.subsystems.GripperSubsystem;
 import frc.robot.subsystems.CartridgeSubsystem;
 import frc.robot.subsystems.armSubsystem;
 import frc.robot.subsystems.collectWheels;
+import frc.robot.subsystems.shootingSubsystem;
 import frc.util.vision.Limelight;
 import frc.robot.commands.ArmCollectCommand;
 import frc.robot.commands.BalanceCommand;
 import frc.robot.commands.LimelightCommand;
-import frc.robot.commands.collectGroupCommand;
+import frc.robot.commands.ShootingCommand;
+// import frc.robot.commands.ClosingCollectGroupCommand;
 import frc.robot.commands.resetCommand;
 import frc.robot.commands.shootingOutputCommand;
 import frc.robot.commands.timercommand.TimerArmPosition;
-import frc.robot.commands.timercommand.TimerGripperCommand;
 import frc.robot.commands.timercommand.moveInParallel;
-import frc.robot.commands.timercommand.openInParallel;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
@@ -35,10 +34,10 @@ public class Next2HumanCommand extends SequentialCommandGroup {
   /** Creates a new Next2Human. */
   // private final Swerve s_Swerve;
   private Swerve s_Swerve;
-  private CartridgeSubsystem ShootingSubsystem;
+  private shootingSubsystem ShootingSubsystem;
+  private CartridgeSubsystem cartridgeSubsystem;
   private CollectSubsystem collectSubsystem;
-  private GripperSubsystem gripperSubsystem;
-  private collectGroupCommand collectGroupCommand;
+  // private ClosingCollectGroupCommand collectGroupCommand;
   private timeSetPointCollectCommand timeSetPointCollectCommand;
   private TimerArmPosition TimerArmPosition;
   private collectWheels collectWheels;
@@ -47,22 +46,22 @@ public class Next2HumanCommand extends SequentialCommandGroup {
 
   public Next2HumanCommand(Swerve swerve,
   CollectSubsystem collectSubsystem,
-  CartridgeSubsystem ShootingSubsystem,
-  GripperSubsystem gripperSubsystem,
-  collectWheels collectWheels, armCollectSubsystem armCollectSubsystem, Limelight limelight) {
+  CartridgeSubsystem cartridgeSubsystem,
+  collectWheels collectWheels, armCollectSubsystem armCollectSubsystem, Limelight limelight, shootingSubsystem shootingSubsystem) {
     // Add your commands in the addCommands() call, e.g.
     this.s_Swerve = swerve;
     this.collectSubsystem = collectSubsystem;
-    this.ShootingSubsystem = ShootingSubsystem;
+    this.cartridgeSubsystem = cartridgeSubsystem;
     this.collectWheels = collectWheels;
     this.limelight = limelight;
     this.armCollectSubsystem = armCollectSubsystem;
+    this.ShootingSubsystem = shootingSubsystem;
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new InstantCommand(() -> swerve.zeroGyro()), new resetCommand(ShootingSubsystem, collectSubsystem, armCollectSubsystem, gripperSubsystem),
-    new shootingOutputCommand(ShootingSubsystem, 0.5, 6930),
+    addCommands(new InstantCommand(() -> swerve.zeroGyro()), new resetCommand(ShootingSubsystem, collectSubsystem, armCollectSubsystem, cartridgeSubsystem),
+    new ShootingCommand(shootingSubsystem, cartridgeSubsystem, 6000, 0.4),
     new moveInParallel(swerve, collectSubsystem, collectWheels, next2human.getAutoCommand(swerve), 290, 5, 1),
     new LimelightCommand(limelight, swerve, true, -1, 0),
-    new shootingOutputCommand(ShootingSubsystem, 0.5, 6930)
+    new ShootingCommand(shootingSubsystem, cartridgeSubsystem, 6000, 0.4)
     );
   }
 }
