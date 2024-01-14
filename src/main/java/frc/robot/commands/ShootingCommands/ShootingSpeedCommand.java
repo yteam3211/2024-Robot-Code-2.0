a05f5756e9d01a2e580486d3074833996332825f
amitaiflint <amitaiflint@gmail.com>
2024-01-15 00:31:49 +0200

    adding isFinish to the commads in complete shooting

--- src/main/java/frc/robot/commands/ShootingCommands/ShootingSpeedCommand.java
+++ src/main/java/frc/robot/commands/ShootingCommands/ShootingSpeedCommand.java
@@ -5,6 +5,7 @@
 package frc.robot.commands.ShootingCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.subsystems.ShootingSubsystem;
 
 public class ShootingSpeedCommand extends Command {
@@ -30,12 +31,19 @@ public class ShootingSpeedCommand extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    shootingSubsystem.setShooterVelocity(0);
+    
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    if (Math.abs(shootingVelocity - shootingSubsystem.getVelocity()) < Constants.SHOOTING_VELOCITY_TRESHOLD) {
+      return true;
+    }
+    else
+    {
+      return false;
+
+    }
   }
 }
